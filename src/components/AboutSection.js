00ad489b3f7c00198233d4e62eb1557006975c37
Arton Mirakaj <artonmirakaj@Arton.lan>
2021-12-30 16:30:34 -0500

    Service Section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -10,10 +10,10 @@ const AboutSection = () => {
             <h2>We work to make</h2>
           </div>
           <div className='hide'>
-            <h2>your <span>dreams</span> come</h2>
+            <h2>your <span>dreams</span></h2>
           </div>
           <div className='hide'>
-            <h2>true.</h2>
+            <h2>come true.</h2>
           </div>
         </div>
         <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
