b3d0d5ccfb100be61ff2fc4b260b653471b62fdb
Arton Mirakaj <artonmirakaj@Arton.lan>
2022-01-07 13:59:53 -0500

    about us animation

--- src/animation.js
+++ src/animation.js
@@ -8,13 +8,50 @@ export const pageAnimation = {
     y: 0,
     transition: {
       duration: 1,
-    }
+      when: 'beforeChildren',
+      staggerChildren: 0.25
+    },
   },
   exit: {
     opacity: 0,
     y: 300,
     transition: {
-      duration: 1,
+      duration: 0.5,
+    }
+  }
+}
+
+export const titleAnim = {
+  hidden: { x: 200, opacity: 0 },
+  show: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.75, ease: 'easeOut' }
+  }
+}
+
+export const fade = {
+  hidden: { x: 100, opacity: 0 },
+  show: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.75 }
+  }
+}
+
+export const container = {
+  hidden: { x: 100 },
+  show: { x: 0, transition: { duration: 0.75, staggerChildren: 1, when: 'beforeChildren' } }
+}
+
+export const photoAnim = {
+  hidden: { scale: 1.5, opacity: 0 },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      ease: 'easeOut',
+      duration: 0.75
     }
   }
 }
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,19 +2,9 @@ import React from 'react'
 import home1 from '../img//home1.png';
 import { About, Description, Image, Hide } from '../styles';
 import { motion } from 'framer-motion';
+import { titleAnim, fade, container, photoAnim } from '../animation';
 
 const AboutSection = () => {
-
-  const titleAnim = {
-    hidden: { opacity: 0},
-    show: { opacity: 1, transition: {duration: 2}}
-  }
-
-  const container = {
-    hidden: { x: 100 },
-    show: { x: 0, transition: { duration: 0.75, staggerChildren: 1, when: 'beforeChildren' } }
-  }
-
   return (
     <About>
       <Description>
@@ -35,11 +25,11 @@ const AboutSection = () => {
             <motion.h2 className='true' variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
-        <button>Contact Us</button>
+        <motion.p variants={fade}>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt='guy with a camera'/>
+        <motion.img variants={photoAnim} src={home1} alt='guy with a camera'/>
       </Image>
     </About>
   )
