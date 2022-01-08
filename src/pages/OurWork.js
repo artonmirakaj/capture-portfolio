39b61e6872ca00ab39dcc21968eda9e7d30aeec5
Arton Mirakaj <artonmirakaj@Arton.lan>
2022-01-08 13:34:37 -0500

    our work page animation

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -5,7 +5,7 @@ import athlete from '../img/athlete-small.png';
 import theracer from '../img/theracer-small.png'
 import goodtimes from '../img/goodtimes-small.png';
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { pageAnimation, fade, photoAnim, lineAnim, slider, sliderContainer } from '../animation';
 
 const OurWork = () => {
   return (
@@ -16,11 +16,19 @@ const OurWork = () => {
       exit='exit'
       style={{ background: '#fff' }}
       >
+        <motion.div variants={sliderContainer}>
+          <Frame1 variants={slider}></Frame1>
+          <Frame2 variants={slider}></Frame2>
+          <Frame3 variants={slider}></Frame3>
+          <Frame4 variants={slider}></Frame4>
+        </motion.div>
       <Movie>
-        <h2>The Athlete</h2>
-        <div className='line' />
+        <motion.h2 variants={fade}>The Athlete</motion.h2>
+        <motion.div variants={lineAnim} className='line' />
         <Link to='/work/the-athlete'>
-          <img src={athlete} alt='athlete' />
+          <Hide>
+            <motion.img variants={photoAnim} src={athlete} alt='athlete' />
+          </Hide>
         </Link>
       </Movie>
       <Movie>
@@ -54,7 +62,7 @@ const Movie = styled.div`
   padding-bottom: 10rem;
   .line{
     height: 0.5rem;
-    background: #cccccc;
+    background: #23D997;
     margin-bottom: 3rem;
   }
   img {
@@ -63,5 +71,31 @@ const Movie = styled.div`
     object-fit: cover;
   }
 `;
+
+const Hide = styled.div`
+  overflow: hidden;
+`
+
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`
+
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`
+
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`
+
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
+`
 
 export default OurWork;
