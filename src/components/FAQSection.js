d6082acfcaca85b025f9e368cb573dbcf653a74a
Arton Mirakaj <artonmirakaj@Arton.lan>
2022-01-08 13:49:57 -0500

    re-usable toggle component

--- src/components/FAQSection.js
+++ src/components/FAQSection.js
@@ -1,43 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
 import { About } from '../styles';
+import Toggle from './Toggle';
 
 const FAQSection = () => {
   return (
     <Faq>
       <h2>Any Questions <span>FAQ</span></h2>
-      <div className='question'>
-        <h4>How Do I Start?</h4>
-        <div className='answer'>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, inventore.</p>
+      <Toggle>
+        <div className='question'>
+          <h4>How Do I Start?</h4>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, inventore.</p>
+          </div>
+          <div className="faq-line" />
         </div>
-        <div className="faq-line" />
-      </div>
-      <div className='question'>
-        <h4>Daily Schedule</h4>
-        <div className='answer'>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, inventore.</p>
+      </Toggle>
+      <Toggle>
+        <div className='question'>
+          <h4>Daily Schedule</h4>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, inventore.</p>
+          </div>
+          <div className="faq-line" />
         </div>
-        <div className="faq-line" />
-      </div>
-      <div className='question'>
-        <h4>Different Payment Methods</h4>
-        <div className='answer'>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, inventore.</p>
+      </Toggle>
+      <Toggle>
+        <div className='question'>
+          <h4>Different Payment Methods</h4>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, inventore.</p>
+          </div>
+          <div className="faq-line" />
         </div>
-        <div className="faq-line" />
-      </div>
-      <div className='question'>
-        <h4>What Products Do You Offer?</h4>
-        <div className='answer'>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, inventore.</p>
+      </Toggle>
+      <Toggle>
+        <div className='question'>
+          <h4>What Products Do You Offer?</h4>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, inventore.</p>
+          </div>
+          <div className="faq-line" />
         </div>
-        <div className="faq-line" />
-      </div>
+      </Toggle>
     </Faq>
   )
 }
